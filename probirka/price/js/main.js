08c14fffcfa41485d9197aff6a73cd166fb2b58e
Linqur <Linqur@gmail.com>
2017-04-12 20:51:17 +0300

    -

--- probirka/price/js/main.js
+++ probirka/price/js/main.js
@@ -1,28 +1,59 @@
-/* строка  при навередниии на ячейки которой будет появляться "крест" */
-  row = document.getElementsByClassName('hover-col');
+/*************************
+**ховерный крест для цен**
+*************************/
 
-/* столбец  при навередниии на ячейки которого будет появляться "крест" */
-  // col = ['col-1','col-2','col-3','col-4'];
+  /* горизонтальная полоса */
+      row = document.getElementsByClassName('hover-col');
 
+      function row_hover(id,e) {
+        $(row[id])
+         .parents( "tr" )
+           .addClass( "active" );
+      }
 
-/* убираем бэкграйнд для tr */
-  function row_hover_off(id,e) {
-    $(row[id])
-     .parents( "tr" )
-       .removeClass( "active" );
-  }
-  for(var i = 0; i < row.length; i++) {
-    row[i].addEventListener('mouseout', row_hover_off.bind(this,i));
-  }
-/* убираем бэкграйнд для tr */
+      function row_hover_off(id,e) {
+        $(row[id])
+         .parents( "tr" )
+           .removeClass( "active" );
+      }
 
-/* добавляем бэкграйнд для tr при ховере на td */
-  function row_hover(id,e) {
-    $(row[id])
-     .parents( "tr" )
-       .addClass( "active" );
-  }
-  for(var i = 0; i < row.length; i++) {
-    row[i].addEventListener('mouseover', row_hover.bind(this,i));
-  }
-/* добавляем бэкграйнд для tr при ховере на td */
+    for(var i = 0; i < row.length; i++) {
+      row[i].addEventListener('mouseout', row_hover_off.bind(this,i));
+      row[i].addEventListener('mouseover', row_hover.bind(this,i));
+    }
+  /*горизонтальная полоса*/
+
+  /*Вертикальная полоса*/
+    col_1 = document.getElementsByClassName('col-1');
+    col_2 = document.getElementsByClassName('col-2');
+    col_3 = document.getElementsByClassName('col-3');
+    col_4 = document.getElementsByClassName('col-4');
+
+    function col_hover(id,e) {
+        for(var i = 0; i < this.length; i++){
+          $(this[i])
+            .addClass('active');
+        }
+    }
+    
+    function col_hover_off(id,e) {
+        for(var i = 0; i < this.length; i++){
+          $(this[i])
+            .removeClass('active');
+        }
+    }
+
+    for(var i = 1; i < 4; i++) {
+      col_1[i].addEventListener('mouseover', col_hover.bind(col_1,i));
+      col_2[i].addEventListener('mouseover', col_hover.bind(col_2,i));
+      col_3[i].addEventListener('mouseover', col_hover.bind(col_3,i));
+      col_4[i].addEventListener('mouseover', col_hover.bind(col_4,i));
+      col_1[i].addEventListener('mouseout', col_hover_off.bind(col_1,i));
+      col_2[i].addEventListener('mouseout', col_hover_off.bind(col_2,i));
+      col_3[i].addEventListener('mouseout', col_hover_off.bind(col_3,i));
+      col_4[i].addEventListener('mouseout', col_hover_off.bind(col_4,i));
+    }
+  /*Вертикальная полоса*/
+/*************************
+**ховерный крест для цен**
+*************************/
